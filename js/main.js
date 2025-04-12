93feafcf8df6359b7dab93958ba21d78bd9e4c0b
Lav-P365 <365lavp@gmail.com>
2025-04-11 22:25:15 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ const app = Vue.createApp({
   data() {
     return {
       teams: [],
+      teamDetails: null,
       owner: null,
       loadingList: true,
       loadingOwner: false,
-      errorList: '',
-      errorOwner: '',
+      errorList: "",
+      errorOwner: ""
     };
   },
 
@@ -16,32 +17,32 @@ const app = Vue.createApp({
 
   methods: {
     fetchTeams() {
-      this.loadingList = true;
       fetch("http://localhost:8000/api/teams")
         .then(res => res.json())
         .then(data => {
           this.teams = data;
         })
         .catch(() => {
-          this.errorList = "⚠️ Failed to fetch teams.";
+          this.errorList = "⚠️ Failed to load teams.";
         })
         .finally(() => {
           this.loadingList = false;
         });
     },
 
-    getTeamDetails(ownerId) {
-      this.loadingOwner = true;
+    getTeamDetails(team) {
+      this.teamDetails = team;
       this.owner = null;
       this.errorOwner = "";
+      this.loadingOwner = true;
 
-      fetch(`http://localhost:8000/api/owner/${ownerId}`)
+      fetch(`http://localhost:8000/api/owner/${team.owner_id}`)
         .then(res => res.json())
         .then(data => {
           this.owner = data;
         })
         .catch(() => {
-          this.errorOwner = "⚠️ Failed to fetch owner info.";
+          this.errorOwner = "⚠️ Failed to load owner details.";
         })
         .finally(() => {
           this.loadingOwner = false;
